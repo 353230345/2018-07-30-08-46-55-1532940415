ba2e6ac917018aa5436f5a13cb91b902ca174e7c
sjq <353230345@qq.com>
2018-08-03 11:19:31 +0800

    res

--- main/main.js
+++ main/main.js
@@ -1,6 +1,11 @@
 module.exports = function main(str) {
   var a=parseInt(str)
-  var sum=0;
-  sum=a/100+(a/10)%10+a%10;
-  return sum;
+  if(str>=0&&str<1000){
+	   var sum=0;
+       sum=a/100+(a/10)%10+a%10;
+       return sum;
+  }else{
+	   break;
+  }
+ 
 };
